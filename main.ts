b9e86d619db6e0b2c1af1ddf7df05cb012746d20
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-09-27 11:20:51 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Emre Guzel 
+ * Created on: Sep 27 2024
+ * This program runs single Led 
 */
+//basic.showString('Hello, World!')
+//setup
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P16,0)
+basic.showIcon(IconNames.Happy)
+
+//trun on LED
+input.onButtonPressed(Button.A,function(){
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16,1)
+})
+//LED of 
+input.onButtonPressed(Button.B,function(){
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16,0)
+})
