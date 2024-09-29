0f83b329f47469cc7d213ce570a8c8fd0c8a92bd
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-09-29 02:27:09 +0000

    Added the trun and of progamr to my cirucut and fofmated my coding

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,20 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Emre Guzell
+Created on: Sep 28 2024
+This module is a Micro:bit MicroPython program runs on and of single LED
 """
 
 from microbit import *
 
+# seting it up
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.get_presses():
+        pin16.write_digital(1)
+    display.show(Image.YES)
+    if button_b.get_presses(0):
+        pin16.write_digital
+    display.show(Image.NO)
